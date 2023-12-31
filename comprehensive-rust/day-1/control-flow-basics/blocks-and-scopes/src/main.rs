641af08868c66f96b852ecd9a9c849a19e89b92b
Artem Nistuley <artem.nistuley@gmail.com>
2023-12-31 14:28:04 +0200

    blocks and scopes

---
+++ comprehensive-rust/day-1/control-flow-basics/blocks-and-scopes/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let z = 13;
+    let x = {
+        let y = 10;
+        println!("y: {y}");
+        z - y
+    };
+    println!("x: {x}");
+
+    let a = 10;
+    println!("before: {a}");
+    {
+        let a ="hello";
+        println!("inner scope: {a}");
+
+        let a = true;
+        println!("shadowed in inner scope: {a}");
+    }
+    println!("after: {a}");
+}
