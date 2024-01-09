a2b3f5753275cfc433cf2037c612ead1338a821a
Artem Nistuley <artem.nistuley@gmail.com>
2024-01-09 12:24:41 +0200

    generics

---
+++ book/10-generics/generics/src/main.rs
@@ -0,0 +1,50 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("{}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("{}", result);
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+    // let wont_work = Point { x: 5, y: 4.0 };
+    let integer_and_float = Point2 { x: 5, y: 4.0 };
+
+    let p = Point { x: 5, y: 10 };
+    println!("p.x = {}", p.x());
+}
+
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item
+        }
+    }
+    largest
+}
