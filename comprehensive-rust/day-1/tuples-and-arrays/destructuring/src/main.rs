5c6bf93006142c9d124516c0d3183f3ac20dc874
Artem Nistuley <artem.nistuley@gmail.com>
2024-04-28 16:39:38 +0300

    update examples to the latest version

--- comprehensive-rust/day-1/tuples-and-arrays/destructuring/src/main.rs
+++ comprehensive-rust/day-1/tuples-and-arrays/destructuring/src/main.rs
@@ -1,31 +1,15 @@
 fn main() {
-    describe_point((-1, -1));
-
-    my_func();
-
-    let arr = [1, 2, 3];
-    match arr {
-        [1, _, _] => println!("First is 1"),
-        _ => ()
-    }
+    print_tuple((10, 20));
+    print_tuple_2((10, 20));
 }
 
-fn describe_point(point: (i32, i32)) {
-    match point {
-        (0, _) => println!("on Y axis"),
-        (_, 0) => println!("on X axis"),
-        (x, _) if x > 0 => println!("left of Y axis"),
-        (_, y) if y < 0 => println!("below X axis"),
-        _ => println!("first quadrant"),
-    }
+fn print_tuple(tuple: (i32, i32)) {
+    let left = tuple.0;
+    let right = tuple.1;
+    println!("left: {left}, right: {right}");
 }
 
-fn my_func() {
-    let triple = [1, -2, 3];
-    println!("Tell me about {triple:?}");
-    match triple {
-        [0, y, z] => println!("First is 0, y = {y}, and z = {z}"),
-        [1, ..] => println!("First is 1 and the rest were ignored"),
-        _ => println!("All elements were ignored"),
-    }
+fn print_tuple_2(tuple: (i32, i32)) {
+    let (left, right) = tuple;
+    println!("left: {left}, right: {right}");
 }
