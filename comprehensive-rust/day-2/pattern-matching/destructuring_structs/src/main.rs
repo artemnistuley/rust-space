8ab76c68cc68e9d2bea6f59c972374be3b601220
Artem Nistuley <artem.nistuley@gmail.com>
2024-05-03 12:56:48 +0300

    destructuring structs

---
+++ comprehensive-rust/day-2/pattern-matching/destructuring_structs/src/main.rs
@@ -0,0 +1,18 @@
+struct Foo {
+    x: (u32, u32),
+    y: u32,
+}
+
+fn main() {
+    let foo = Foo {
+        x: (1, 2),
+        y: 3,
+    };
+
+    match foo {
+        Foo { x: (a, 2), y } => println!("x.1 = 2, a = {a}, y = {y}"),
+        Foo { x: (1, b), y } => println!("x.0 = 1, b = {b}, y = {y}"),
+        Foo { y: 2, x: i }   => println!("y = 2, x = {i:?}"),
+        Foo { y, .. }        => println!("y = {y}, other fields were ignored"),
+    }
+}
