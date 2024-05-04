daec4957aa3d5c3eaca5e849d440a9f9ef733ed8
Artem Nistuley <artem.nistuley@gmail.com>
2024-05-04 10:27:30 +0300

    traits

---
+++ comprehensive-rust/day-2/methods-and-traits/traits/src/main.rs
@@ -0,0 +1,9 @@
+trait Pet {
+    /// Return a sentence from this pet.
+    fn talk(&self) -> String;
+
+    /// Print a string to the terminal greeting this pet.
+    fn greet(&self);
+}
+
+fn main() {}
