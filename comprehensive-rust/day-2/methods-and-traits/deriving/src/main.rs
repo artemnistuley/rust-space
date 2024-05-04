9e2b836533ed1ef53e60804281cd40116edd40e1
Artem Nistuley <artem.nistuley@gmail.com>
2024-05-04 11:30:00 +0300

    deriving

---
+++ comprehensive-rust/day-2/methods-and-traits/deriving/src/main.rs
@@ -0,0 +1,14 @@
+#[derive(Debug, Clone, Default)]
+struct Player {
+    name: String,
+    strength: u8,
+    hit_points: u8,
+}
+
+fn main() {
+    let p1 = Player::default();
+    let mut p2 = p1.clone();
+    p2.name = String::from("Joe Doe");
+
+    println!("{:?} vs. {:?}", p1, p2);
+}
