6c9c2632daba8ea01455e44a98410e783a232f6f
Artem Nistuley <artem.nistuley@gmail.com>
2024-05-06 12:51:51 +0300

    exercise generic min

---
+++ comprehensive-rust/day-2/generics/exercise_generic_min/src/main.rs
@@ -0,0 +1,29 @@
+use std::cmp::Ordering;
+
+// fn min<T>(a: T, b: T) -> T
+// where
+//     T: PartialOrd  
+// {
+//     if a < b { a } else { b }
+// }
+
+fn min<T>(a: T, b: T) -> T
+where
+    T: Ord  
+{
+    match a.cmp(&b) {
+        Ordering::Less | Ordering::Equal => a,
+        Ordering::Greater => b,
+    }
+}
+
+fn main() {
+    assert_eq!(min(0, 10), 0);
+    assert_eq!(min(500, 123), 123);
+
+    assert_eq!(min('a', 'z'), 'a');
+    assert_eq!(min('7', '1'), '1');
+
+    assert_eq!(min("hello", "goodbye"), "goodbye");
+    assert_eq!(min("bat", "armadillo"), "armadillo");
+}
