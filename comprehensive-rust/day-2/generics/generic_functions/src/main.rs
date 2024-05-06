dae5c243b9d3bcf0aa2110618203141a76f055f3
Artem Nistuley <artem.nistuley@gmail.com>
2024-05-06 11:55:40 +0300

    generic functions

---
+++ comprehensive-rust/day-2/generics/generic_functions/src/main.rs
@@ -0,0 +1,8 @@
+fn pick<T>(n: i32, even: T, odd: T) -> T {
+    if n % 2 == 0 { even } else { odd }
+}
+
+fn main() {
+    println!("picked a number: {:?}", pick(97, 222, 333));
+    println!("picked a tuple: {:?}", pick(28, ("dog", 1), ("cat", 2)));
+}
