9f5e97a494f11abddd17a7f80afac6ed48acab9a
Artem Nistuley <artem.nistuley@gmail.com>
2024-07-11 17:09:18 +0300

    try conversions

---
+++ comprehensive-rust/day-4/error_handling/try_conversions/src/main.rs
@@ -0,0 +1,42 @@
+use std::error::Error;
+use std::fmt::{self, Display, Formatter};
+use std::fs::File;
+use std::io::{self, Read};
+
+#[derive(Debug)]
+enum ReadUsernameError {
+    IoError(io::Error),
+    EmptyUsername(String),
+}
+
+impl Error for ReadUsernameError {}
+
+impl Display for ReadUsernameError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Self::IoError(e) => write!(f, "IO error: {e}"),
+            Self::EmptyUsername(path) => write!(f, "Found no username in {path}"),
+        }
+    }
+}
+
+impl From<io::Error> for ReadUsernameError {
+    fn from(err: io::Error) -> Self {
+        Self::IoError(err)
+    }
+}
+
+fn read_username(path: &str) -> Result<String, ReadUsernameError> {
+    let mut username = String::with_capacity(100);
+    File::open(path)?.read_to_string(&mut username)?;
+    if username.is_empty() {
+        return Err(ReadUsernameError::EmptyUsername(String::from(path)));
+    }
+    Ok(username)
+}
+
+fn main() {
+    //std::fs::write("config.dat", "").unwrap();
+    let username = read_username("config.dat");
+    println!("username or error: {username:?}");
+}
