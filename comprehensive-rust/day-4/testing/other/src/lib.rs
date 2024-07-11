1927de00bd2f23d4d4178ed022b0ed46283a70e4
Artem Nistuley <artem.nistuley@gmail.com>
2024-07-11 13:43:37 +0300

    other tests

---
+++ comprehensive-rust/day-4/testing/other/src/lib.rs
@@ -0,0 +1,9 @@
+/// Add func.
+///
+/// ```
+/// # use other::add;
+/// assert_eq!(add(2, 2), 4);
+/// ```
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
---
+++ comprehensive-rust/day-4/testing/other/tests/my_library.rs
@@ -0,0 +1,6 @@
+use other::add;
+
+#[test]
+fn test_add() {
+  assert_eq!(add(2, 2), 4)
+}
