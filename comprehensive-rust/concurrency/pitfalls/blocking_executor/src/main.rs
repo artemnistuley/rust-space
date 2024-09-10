d03bd03c6cb582d1f890ae227f5543a6497e0f7c
Artem Nistuley <artem.nistuley@gmail.com>
2024-09-10 14:49:10 +0300

    added a comment

--- comprehensive-rust/concurrency/pitfalls/blocking_executor/src/main.rs
+++ comprehensive-rust/concurrency/pitfalls/blocking_executor/src/main.rs
@@ -2,6 +2,8 @@ use futures::future::join_all;
 use std::time::Instant;
 use tokio::time;
 
+// https://google.github.io/comprehensive-rust/concurrency/async-pitfalls/blocking-executor.html
+
 async fn sleep_ms(start: &Instant, id: u64, duration_ms: u64) {
     // std::thread::sleep(std::time::Duration::from_millis(duration_ms));
     time::sleep(time::Duration::from_millis(duration_ms)).await;
