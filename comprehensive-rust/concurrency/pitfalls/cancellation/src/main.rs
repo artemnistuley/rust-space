a050d8e458d89be44b09f843df83179f2b09eb7f
Artem Nistuley <artem.nistuley@gmail.com>
2024-09-10 15:09:44 +0300

    async traits and cancellation

---
+++ comprehensive-rust/concurrency/pitfalls/cancellation/src/main.rs
@@ -0,0 +1,64 @@
+use std::io::{self, ErrorKind};
+use std::time::Duration;
+use tokio::io::{AsyncReadExt, AsyncWriteExt, DuplexStream};
+
+// https://google.github.io/comprehensive-rust/concurrency/async-pitfalls/cancellation.html
+
+struct LinesReader {
+    stream: DuplexStream,
+    bytes: Vec<u8>,
+    buf: [u8; 1],
+}
+
+impl LinesReader {
+    fn new(stream: DuplexStream) -> Self {
+        Self { stream, bytes: Vec::new(), buf: [0] }
+    }
+
+    async fn next(&mut self) -> io::Result<Option<String>> {
+        // let mut bytes = Vec::new();
+        // let mut buf = [0];
+        while self.stream.read(&mut self.buf[..]).await? != 0 {
+            self.bytes.push(self.buf[0]);
+            if self.buf[0] == b'\n' {
+                break;
+            }
+        }
+        if self.bytes.is_empty() {
+            return Ok(None);
+        }
+        let raw = std::mem::take(&mut self.bytes);
+        let s = String::from_utf8(raw)
+            .map_err(|_| io::Error::new(ErrorKind::InvalidData, "not UTF-8"))?;
+        Ok(Some(s))
+    }
+}
+
+async fn slow_copy(source: String, mut dest: DuplexStream) -> std::io::Result<()> {
+    for b in source.bytes() {
+        dest.write_u8(b).await?;
+        tokio::time::sleep(Duration::from_millis(10)).await
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let (client, server) = tokio::io::duplex(5);
+    let handle = tokio::spawn(slow_copy("hi\nthere\n".to_owned(), client));
+
+    let mut lines = LinesReader::new(server);
+    let mut interval = tokio::time::interval(Duration::from_millis(60));
+    loop {
+        tokio::select! {
+            _ = interval.tick() => println!("tick!"),
+            line = lines.next() => if let Some(l) = line? {
+                print!("{}", l)
+            } else {
+                break
+            },
+        }
+    }
+    handle.await.unwrap()?;
+    Ok(())
+}
